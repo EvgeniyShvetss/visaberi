fdb7ac293711618a980575573ee47a19857be67f
shvetsevgen <49642659+shvetsevgen@users.noreply.github.com>
2019-06-02 13:00:16 +0300

    message

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,13 @@
-$(function() {
+$(function () {
 
+	(function () {
+		var triger = $('.country__triger-btn');
 
+		triger.on('click', function () {
+			$('.country__item-additionally').slideToggle(1000);
+			$(this).toggleClass('rotate');
+
+		});
+	}());
 
 });
